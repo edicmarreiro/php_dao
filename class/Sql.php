733c370abd7c70dfae740a768e5b43069122fd1d
Edi Marreiro <edimarreiro@hotmail.com>
2021-12-01 14:16:57 -0300

    iniciamos o projeto

---
+++ class/Sql.php
@@ -0,0 +1,56 @@
+<?php 
+
+class Sql extends PDO {
+
+  private $conn;
+
+  /*public function __construct(){
+
+    $this->conn = new PDO("mysql:host=localhost; bdname=php_curso" , "root" ,"");
+
+  }*/
+
+  public function __construct()
+{
+     $this->conn = new \PDO(
+         "mysql:dbname=php_curso;host=localhost", 
+         "root",
+         "",
+         array(
+             \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+         )
+     );
+}
+
+  private function setParams($statment, $parameters = array()){
+ 
+    foreach ($parameters as $key => $value){
+        $this->setParam($key, $value);
+    }
+   
+  }
+
+  private function setParam($statment, $key, $value){
+      $statment->bindParam($key, $value);
+  }
+
+  public function query($rawQuery, $params = array()){
+    $stmt = $this->conn->prepare($rawQuery);
+
+    $this->setParams($stmt, $params);
+
+    $stmt->execute();
+
+    return $stmt;
+
+  }
+
+  public function select($rawQuery, $params = array()):array
+  {
+      $stmt = $this->query($rawQuery, $params);
+       return $stmt->fetchAll(PDO:: FETCH_ASSOC);
+  }
+
+}
+
+?>
